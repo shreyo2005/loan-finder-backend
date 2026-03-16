eba8410d1cc38894fb3d0698404c881c4423710b
shreyo2005 <diya.laik2005@gmail.com>
2026-03-16 22:14:56 +0530

    mongoDB connected, dotenv figured

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express =require('express');
+const dotenv=require('dotenv')
+const connectDB=require('./config/db')
 const loanroutes=require('./routes/loanroutes')
+
+dotenv.config()
+connectDB()
+
 const app=express()
 app.use(express.json())  //middleware to read json data in the request body and make it available to us 
 app.use('/api/loans',loanroutes)
@@ -9,7 +15,7 @@ app.post('/test', (req, res) => {
 })
 
 
-const PORT=3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
   console.log(`server is running on port ${PORT}`)
 })
