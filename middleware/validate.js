d93d360869043bd70658d64009d2526328ad15e6
shreyo2005 <diya.laik2005@gmail.com>
2026-03-24 16:29:14 +0530

    improved validate.js messages rewritten

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,29 +1,56 @@
 //loanroutes.js -> validate.js -> loancontroller.js
 
-const validate=(req,res,next)=>{
-    const{purpose,amount}=req.body
+// const validate=(req,res,next)=>{
+//     const{purpose,amount}=req.body
+//     if(!purpose){
+//         return res.status(400).json({message:'purpose is required'})
+//     }
+//     if(!amount){
+//         return res.status(400).json({message:'amount is required'})
+//     }
+//     if(typeof amount!=='number'){
+//         return res.status(400).json({message:'amount must be number'})
+//     }
+//     if(amount<=0){
+//         return res.status(400).json({message:'amount must be greater than zero'})
+//     }
+//     const validpurpose=['education','home','business','vehicle','personal']
+//     if(!validpurpose.includes(purpose)){
+//         return res.status(400).json({
+//             message:`purpose must be one of: ${validpurpose.join(', ')}`
+//         })
+//     }
+//     next()
+
+// }
+
+// module.exports=validate  //------> this is manual validation
+
+const express=require("express")
+const validate=(req,res,body)=>{
+    const {purpose,amount}=req.body
     if(!purpose){
         return res.status(400).json({message:'purpose is required'})
     }
     if(!amount){
-        return res.status(400).json({message:'amount is required'})
+        return res.status(400).json({message:"amount is required"})
     }
-    if(typeof amount!=='number'){
-        return res.status(400).json({message:'amount must be number'})
+    if(typeof amount !=='Number'){
+        return res.status(400).json({message:"the amount must be a number"})
     }
     if(amount<=0){
-        return res.status(400).json({message:'amount must be greater than zero'})
+        return res.status(400).json({message:"the amount must be greater than zero"})
     }
     const validpurpose=['education','home','business','vehicle','personal']
     if(!validpurpose.includes(purpose)){
         return res.status(400).json({
-            message:`purpose must be one of: ${validpurpose.join(', ')}`
+             message:`purpose must be one of: ${validpurpose.join(', ')}`
         })
     }
     next()
 
+
+
 }
-
-module.exports=validate  //------> this is manual validation
-
+module.exports=validate 
 
